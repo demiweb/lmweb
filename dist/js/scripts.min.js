09a2a47168dbf5cb444f631b8dec0906669cf2a4
JukaChu <olexiy.yankevich@gmail.com>
2022-07-06 18:35:54 +0300

    comm

--- dist/js/scripts.min.js
+++ dist/js/scripts.min.js
@@ -301,6 +301,70 @@ scroSlider();
 //swipers
 //animations all
 
+//image 3d rotating
+
+
+
+// $('.cardBottom').mouseleave(function(){
+//     $('.card').addClass('restore');
+// });
+function obj_hover_rotate($hover_obj, $wrap_obj, $move_obj){
+    $($hover_obj).mousemove(function(ev){
+        // var clientHeight = $(window).height;
+        // var clientWidth = $(window).width;
+        // $('.card').removeClass('restore')
+        var oEvent = ev || event;
+
+
+        var cardWidth = parseInt($($wrap_obj).css('width'));
+        var cardHeight = parseInt($($wrap_obj).css('height'));
+        var cardLeft = window.innerWidth / 2;
+
+        var cardTop = parseInt(document.querySelector($wrap_obj).getBoundingClientRect().top);
+
+        // console.log(oEvent.clientX + ' left = ' + cardTop);
+        var centerDisX = oEvent.clientX - cardLeft;
+        var centerDisY = oEvent.clientY - cardTop;
+        // console.log(centerDisX)
+
+
+        //$('.txt').val(centerDisX + ", "+ centerDisY);
+        var degX = (Math.abs(centerDisY) / (cardHeight/2)) * 8;
+        var degY = (Math.abs(centerDisX) / (cardWidth/2)) * 18;
+        //$('.txt').val(degX + ", "+ degY);
+
+        if(centerDisY < 0 && centerDisX < 0){
+            $($move_obj).css({'transform' : 'translate(0, 0) rotateX('+degX+'deg) rotateY(-'+degY+'deg)'});
+        }
+        if(centerDisY < 0 && centerDisX > 0){
+            $($move_obj).css({'transform' : 'translate(0, 0) rotateX('+degX+'deg) rotateY('+degY+'deg)'});
+        }
+        if(centerDisY > 0 && centerDisX < 0){
+            $($move_obj).css({'transform' : 'translate(0, 0) rotateX(-'+degX+'deg) rotateY(-'+degY+'deg)'});
+        }
+        if(centerDisY > 0 && centerDisX > 0){
+            $($move_obj).css({'transform' : 'translate(0, 0) rotateX(-'+degX+'deg) rotateY('+degY+'deg)'});
+        }
+    });
+}
+
+
+function ifGetRotatingImage() {
+    if (document.querySelector('.dev-tools__pic')) {
+        obj_hover_rotate("body", ".dev-tools__pic", ".img-rot");
+
+    } else {
+
+    }
+}
+ifGetRotatingImage();
+//image 3d rotating
+
+//grid items photo
+
+
+//grid items photo
+
 window.addEventListener('mousemove', () => {
 
 });
